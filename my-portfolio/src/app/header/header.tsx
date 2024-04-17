432e522b7722bf8e7a545db503d115186e754874
annythingelse <fernanda_g_santana@estudante.sesisenai.org.br>
2024-04-17 17:18:06 -0300

    feat: header

---
+++ my-portfolio/src/app/header/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <div>
+        <div>{/* <img src={require("")} alt="" /> */}</div>
+        <h1>teste</h1>
+      </div>
+    </>
+  );
+}
--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,3 +1,12 @@
+import React from "react";
+import Header from "./header/header";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
+  );
 }
