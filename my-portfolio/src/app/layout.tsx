af78b391a65c0955dc40b1ce3962014776885c2e
Fernanda Guimarães Santana <fernandaguimaraes0202@gmail.com>
2024-05-15 15:22:28 -0300

    ajustando detalhes

--- my-portfolio/src/app/layout.tsx
+++ my-portfolio/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Jost } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const jost = Jost({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "My portfolio",
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>
+      <body className={jost.className}>
         <link rel="icon" href="./favicon.ico" />
         {children}
       </body>
--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -9,11 +9,17 @@ export default function Home() {
         <Header />
         <div className="home-container">
           <div className="home-infocard">
+            <div className="infocard-image">
+              <img src="/images/annythingelse.jpg" alt="annythingelse" />
+            </div>
             <div className="infocard-links">
-              <h2 className="infocard-title">Fernanda Guimarães</h2>
+              <h2 className="infocard-title">Front-end Developer</h2>
               <p className="infocard-text">
-                Olá! Seja bem vindo(a) ao meu portfólio! Cheque o meu currículo
-                clicando no botão a baixo, se preferir.
+                Olá Mundo! Seja bem vindo(a) ao meu portfólio! Meu nome é
+                Fernanda Guimarães Santana, sou desenvolvedora a mais ou menos 3
+                anos, tenho experiência sólida no front-end, principalmente
+                utilizando o ReactJS. Se quiser saber mais de forma prática ↪
+                cheque o meu currículo clicando no botão a baixo ↓
               </p>
               <button className="infocard-button">CV</button>
             </div>
