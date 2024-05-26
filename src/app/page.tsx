7654a986129082294351bff14ea2e0e3e879dce3
annythingelse <fernandaguimaraes0202@gmail.com>
2024-05-26 11:07:19 -0300

    ajust: size of header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "../components/header/";
 import Footer from "@/components/footer";
 import "./style.scss";
 import { History } from "@/components/accordion/content";
+
 export default function Home() {
   return (
     <>
