e4d6d7a33bca79dd94451d27aa636804b53b5682
Fernanda Guimarães Santana <87783154+annythingelse@users.noreply.github.com>
2024-05-27 18:00:06 -0300

    Atualizar o page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
                 <h4 className="experience-name h4-title">Avanti</h4>
                 <p className="subtitle"> 09/2021 - 07/2023</p>
                 <p className="experience-description paragraph-text">
-                  Desenvolvimento de projetos utilizando a plataforma VTEX.IO como:
+                  Desenvolvimento de sites de e-commerce utilizando a plataforma VTEX.IO como:
                   Fila, PenseFarma, Kopenhagen e Mizuno.
                 </p>
               </div>
